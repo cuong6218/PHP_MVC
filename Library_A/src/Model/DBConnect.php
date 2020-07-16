ac18307aa376610d01e7d4b5741fd60e703fdc38
cuong6218 <cuong6218@domain.com>
2020-07-16 17:00:38 +0700

    tao file ket noi database

---
+++ Library_A/src/Model/DBConnect.php
@@ -0,0 +1,23 @@
+<?php
+    namespace App\Model;
+
+use PDOException;
+
+class DBConnect{
+        private $dsn;
+        private $username;
+        private $password;
+        function __construct()
+        {
+            $this->dsn = 'mysql:host=localhost;dbname=library_data;charset=utf8';
+            $this->username = 'root';
+            $this->password = 'manhcuong123';
+        }
+        function connect(){
+            try{
+                return new PDO($this->dsn, $this->username, $this->password);
+            } catch (PDOException $e){
+                echo $e->getMessage();
+            }
+        }
+    }
