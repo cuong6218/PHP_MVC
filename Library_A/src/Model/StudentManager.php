e34a18d2cbe6bf234e128deac4dba4583a9e52c1
cuong6218 <cuong6218@domain.com>
2020-07-16 17:19:30 +0700

    them chuc nang hien thi du lieu database trong file studentmanager

---
+++ Library_A/src/Model/StudentManager.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model;
+
+include_once('DBConnect.php');
+include_once('Student.php');
+class StudentManager
+{
+    protected $database;
+    function __construct()
+    {
+        $db = new DBConnect();
+        $this->database = $db->connect();
+    }
+    function getAllBook()
+    {
+        $sql = "SELECT * FROM students";
+        $stmt = $this->database->query($sql);
+        $data = $stmt->fetchAll();
+        $arr = [];
+        foreach ($data as $human) {
+            $student = new Student($human['name'], $human['class'], $human['phone'], $human['email']);
+            $student->setId($human['id']);
+            array_push($arr, $student);
+        }
+        return $arr;
+    }
+}
